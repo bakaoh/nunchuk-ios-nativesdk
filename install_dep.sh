3995eaf71c537cfb19a103512bf21e4c61fe7c5a
Bakaoh <bakaoh@gmail.com>
2025-12-05 18:40:09 +0700

    Add CMakeLists patch

--- install_dep.sh
+++ install_dep.sh
@@ -33,6 +33,7 @@ applyBitcoinDependsPatches() {
   patchFile ./patches/boost.mk ./NunchukSDK/libnunchuk/contrib/bitcoin/depends/packages/boost.mk
   patchFile ./patches/ios.mk ./NunchukSDK/libnunchuk/contrib/bitcoin/depends/hosts/ios.mk
   patchFile ./patches/Makefile ./NunchukSDK/libnunchuk/contrib/bitcoin/depends/Makefile
+  patchFile ./patches/CMakeLists.txt ./NunchukSDK/libnunchuk/contrib/bitcoin/src/CMakeLists.txt
   patchFile ./patches/ProcessConfigurations.cmake ./NunchukSDK/libnunchuk/contrib/bitcoin/cmake/module/ProcessConfigurations.cmake
   patchFile ./patches/AddBoostIfNeeded.cmake ./NunchukSDK/libnunchuk/contrib/bitcoin/cmake/module/AddBoostIfNeeded.cmake
   patchFile ./patches/FindLibevent.cmake ./NunchukSDK/libnunchuk/contrib/bitcoin/cmake/module/FindLibevent.cmake
